e77c5a8f1285ab341077cf40a1a20d3212b12ed6
AKMAL HAKIM BIN JAMALUDDIN <141492726+akmalhakim05@users.noreply.github.com>
2025-05-21 02:10:22 +0800

    Upload

--- Create database.php
+++ Create database.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
 
 // Create database
 $sql = "CREATE DATABASE Cafe_Inventory";
-if ($conn->query ($sq1) === TRUE) {
+if ($conn->query ($sql) === TRUE) {
 echo "Database created successfully";
 } else {
 echo "Error creating database: " . $conn->error;
